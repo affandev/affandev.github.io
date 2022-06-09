3b3d1c47d462d3f24702195089d3baf5fb1c2914
mhusendev <husenrgc.96@gmail.com>
2022-06-09 12:47:29 +0700

    update

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const base_url = 'https://a7c5-110-138-54-163.ap.ngrok.io'
+const base_url = 'https://7217-110-138-54-163.ap.ngrok.io'
 // params
 const save = '/generateStruk';
 const route_login ='/api/login'
